7229a9c47473aabaae753d8b612a190fa19d6830
DracoR22 <rdraco039@gmail.com>
2024-04-06 17:58:30 -0500

    first commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { add } from "@suply-chain/sample-lib"
+
+const HomePage = () => {
+
+    console.log('add', add(24, 24))
+
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default HomePage
